3493ba0517928477a9da38718ff100d3d8e6d120
VladAver <Averianov87@gmail.com>
2019-02-11 18:32:11 +0300

    get method

--- LinkedListLibrary/src/main/java/com/vladaver87/LinkedListLibrary/List.java
+++ LinkedListLibrary/src/main/java/com/vladaver87/LinkedListLibrary/List.java
@@ -11,13 +11,32 @@ public class List<T> {
 	}
 
 	public int size() {
-		int count = 0;	
+		int count = 0;
 		element = first;
-		while(element != null) {
+		while (element != null) {
 			element = element.getNext();
 			count++;
 		}
 		return count;
+	}
+
+	public Element<T> get(int i) {
+		int count = 0;
+		if (i == 0) return first;
+		element = first;
+		while (element != null) {
+			element = element.getNext();
+			count++;
+			if (count == i)
+			{
+				break;
+			}
+			else 
+			{
+				element = null;
+			}
+		}
+		return element;
 	}
 
 	public void add(T value) {
@@ -31,11 +50,12 @@ public class List<T> {
 		}
 	}
 
+	
 	public static void main(String[] args) {
 		List<String> list = new List<String>(null, null);
 		list.add("one");
 		list.add("two");
-		System.out.print("size" +list.size());
+		System.out.print("Element" + list.get(0));
 	}
 
 }
