5905c50c06dd2b0cfac14a914ca51c8146fe2d00
VladAver <Averianov87@gmail.com>
2019-02-12 19:57:30 +0300

    test methods add

--- LinkedListLibrary/src/test/java/com/vladaver87/LinkedListLibrary/ListTest.java
+++ LinkedListLibrary/src/test/java/com/vladaver87/LinkedListLibrary/ListTest.java
@@ -6,12 +6,26 @@ import org.junit.Test;
 
 public class ListTest {
 	List<String> list = new List<String>(null, null);
+	
 
 	@Test
 	public void testInserElement() {
 		assertNull(list.get(0));		
 		
 	}
-
+	@Test
+	public void addElement() {
+		list.add("one");
+		list.add("two");
+		list.add("three");
+		list.add("four");
+	}
+	@Test
+	public void checkElementOrder() {
+		assertEquals(list.get(0), "four");
+		assertEquals(list.get(1), "three");
+		assertEquals(list.get(2), "two");
+		assertEquals(list.get(3), "one");
+	}
 
 }
