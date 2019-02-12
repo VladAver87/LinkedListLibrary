e0e83ec67e6494c5aecc951c198f008d5545bd8f
VladAver <Averianov87@gmail.com>
2019-02-12 22:40:24 +0300

    add toString method

--- LinkedListLibrary/src/main/java/com/vladaver87/LinkedListLibrary/List.java
+++ LinkedListLibrary/src/main/java/com/vladaver87/LinkedListLibrary/List.java
@@ -4,6 +4,16 @@ public class List<T> {
 	private Element<T> element;
 	private Element<T> first;
 
+	public String toString() {;
+		element = first;
+		String result = "";
+		while (element != null) {
+			result += element.getValue() + "\n";
+			element = element.getNext();	
+		}
+		return result;
+	}
+
 	public int size() {
 		int count = 0;
 		element = first;
@@ -15,7 +25,8 @@ public class List<T> {
 	}
 
 	public T get(int i) {
-		if (first == null) return null;
+		if (first == null)
+			return null;
 		int count = 0;
 		if (i == 0)
 			return first.getValue();
@@ -39,5 +50,14 @@ public class List<T> {
 			element.setNext(first);
 			first = element;
 		}
+	}
+
+	public static void main(String[] args) {
+		List<String> list = new List<String>();
+		list.add("one");
+		list.add("two");
+		list.add("three");
+		list.add("four");
+		System.out.println(list.toString());
 	}
 }
